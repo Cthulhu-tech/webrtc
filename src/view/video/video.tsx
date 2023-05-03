1888883f94da8fb681f701474423878d38fc7737
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2023-05-03 18:30:29 +0400

    update

--- src/view/video/video.tsx
+++ src/view/video/video.tsx
@@ -1,4 +1,18 @@
+import { io } from 'socket.io-client'
+import { useEffect } from "react"
+
+const socket = io('http://localhost:3001/', {
+    withCredentials: true,
+})
+
 export const Video = () => {
 
-    return <>video</>
+    useEffect(() => {
+        socket.emit('JOIN_ROOM', {
+            room_id: 8,
+            user_id: 5
+        })
+    },[])
+
+    return <video id="localVideo" autoPlay playsInline />
 }

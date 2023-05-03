67f6f109edbd20e968305def7432a165fa8cd609
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2023-05-03 16:01:06 +0400

    router add

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,5 +1,14 @@
-import { createHashRouter } from "react-router-dom"
+import { createBrowserRouter } from "react-router-dom"
+import { Create } from "../view/create/create"
+import { Video } from "../view/video/video"
 
-export const router = createHashRouter([
-
+export const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Create/>
+    },
+    {
+        path: '/video',
+        element: <Video/>
+    }
 ])

767f710b92eed4f1c1bee9afc7430bf86c6fcf35
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2023-05-05 08:26:30 +0400

    update

--- src/view/video/video.tsx
+++ src/view/video/video.tsx
@@ -5,29 +5,32 @@ const socket = io('http://localhost:3001/', {
     withCredentials: true,
 })
 
+let dataChannels: { [key:string]: RTCDataChannel } = {}
 let peerConnections: { [key:string]: RTCPeerConnection } = {}
 
 export const Video = () => {
 
     const createRTC = (user: string) => {
-        const peerConnection = new RTCPeerConnection()
+        const peerConnection = new RTCPeerConnection(undefined)
         peerConnection.onicecandidate = (e) => 
             socket.emit('SEND_ICE_CANDIDATE', {
                 candidate: e.candidate,
                 user,
             })
-        
-        navigator.mediaDevices
-            .getUserMedia({
-                audio: true,
-                video: true,
-            })
-            .then((localStream) => {
+        navigator.mediaDevices.getUserMedia({
+            video: false,
+            audio: true,
+        }).then((stream) => {
+            stream.getTracks().forEach(track => {
+                const videoContainer = document.querySelector('.video')
                 const video = document.createElement('video')
-                video.srcObject = localStream
-                const divVideo = document.querySelector('.video') as HTMLDivElement
-                divVideo.append(video)
+                video.autoplay = true
+                video.id = user
+                video.srcObject = stream
+                videoContainer?.append(video)
+                peerConnection.addTrack(track, stream)
             })
+        })
         return peerConnection
     }
 
@@ -36,7 +39,20 @@ export const Video = () => {
         // клиент зашёл
         socket.on('RECEIVE_CLIENT_JOINED', ({ user_server_id }) => {
             const peerConnection = createRTC(user_server_id)
-            peerConnection.createOffer()
+            const dataChannel = peerConnection.createDataChannel(user_server_id)
+
+            dataChannel.addEventListener('message', event => console.log(event.data))
+
+            dataChannel.onopen = () => {
+                dataChannel.send('message')
+            }
+            
+            dataChannels[user_server_id] = dataChannel
+
+            peerConnection.createOffer({
+                offerToReceiveVideo: false,
+                offerToReceiveAudio: true,
+            })
             .then((offer) => {
                 peerConnection.setLocalDescription(offer)
                 socket.emit('SEND_OFFER', {
@@ -50,14 +66,35 @@ export const Video = () => {
         socket.on('RECEIVE_OFFER', ({ offer, user }) => {
             const peerConnection = createRTC(user)
             peerConnection.setRemoteDescription(offer)
+
+            peerConnection.ontrack = (event) => {
+                const videoContainer = document.querySelector('.video')
+                const video = document.createElement('video')
+                const [remoteStream] = event.streams
+                video.autoplay = true
+                video.id = user
+                video.srcObject = remoteStream
+                videoContainer?.append(video)
+            }
+
             peerConnection.createAnswer()
             .then((answer) => {
                 peerConnection.setLocalDescription(answer)
-                socket.emit('send answer', {
+                socket.emit('SEND_ANSWER', {
                     answer,
                     user
                 })
             })
+
+            peerConnection.ondatachannel = (e) => {
+                const dataChannel = e.channel
+    
+                dataChannel.onmessage = (message) => {
+                    console.log(message.data)
+                }
+                dataChannels[user] = dataChannel
+            }
+
             peerConnections[user] = peerConnection
         })
         // данные на отправку к подключению
@@ -69,6 +106,6 @@ export const Video = () => {
     },[])
 
     return <div className='video'>
-
+        
     </div>
 }
